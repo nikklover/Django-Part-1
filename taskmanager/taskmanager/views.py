76f1b846aee30b4ceec3c775cc7280a42e5c759f
nikolay <stark-nick10@mail.ru>
2021-08-08 02:29:57 +0300

    keys username

--- taskmanager/taskmanager/views.py
+++ taskmanager/taskmanager/views.py
@@ -4,3 +4,8 @@ def about(request):
 	return render(request,'about.html')
 def home(request):
 	return render(request,'home.html')
+def hoba(request):
+	user_text=request.GET['usertext']
+	r_text=user_text[::-1]
+	return render(request,'hoba.html',{'usertext':user_text})
+
